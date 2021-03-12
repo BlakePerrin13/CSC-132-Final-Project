564cd9f2a884884ebfe7ce252516101fcf463252
Cameron Thomas <cameron102601@gmail.com>
2021-03-12 10:22:46 -0600

    Added to student2Function and main part of program

--- CSC-132-Final-Project.py
+++ CSC-132-Final-Project.py
@@ -2,6 +2,10 @@ def student1Function():
     pass
 
 def student2Function():
-    pass
+    print("Something interesting")
+    print(2 + 2)
 
 ################# Main Program ################
+
+student1Function()
+student2Function()
