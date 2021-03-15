2335da43096c3e8f6d6e979d333e7a8f375f5b0a
Cameron Thomas <cameron102601@gmail.com>
2021-03-15 15:34:57 -0500

    Added comments and cleaned up code a bit. Also added buttons but they are currently not functional

--- FinalProject/GameProject.py
+++ FinalProject/GameProject.py
@@ -1,39 +1,57 @@
 # main game plays here
 
+# import pygame and image files
 import pygame as pyg
 import imgs
 
 
+# initiate pygame
 pyg.init()
 
+# set display dimensions
 display_width = 800
 display_height = 600
 
+# initiate display and caption
 gameDisplay = pyg.display.set_mode((display_width, display_height))
 pyg.display.set_caption('Blackjack')
 
-black = (0, 0, 0)
-white = (255, 255, 255)
+# set color values
 green = (34, 99, 43)
 
+# initiates clock and end parameter
 clock = pyg.time.Clock()
 end = False
 
+# how much the cards should be offset in x and y directions when new card is dealt
+card_offset = 40
 
+
+# defines function to display images (might be redundant whoops)
 def display_img(img, x, y):
     gameDisplay.blit(img, (x, y))
 
 
-x = (display_width * 0.35)
-y = (display_height * 0.56)
+# defines setup function to be run at start of loop
+def setup():
+    gameDisplay.fill(green)
+    display_img(imgs.AC, card1_x, card1_y)
+    display_img(imgs.deal, display_width * 0.9, display_height * 0.87)
+    display_img(imgs.hit, display_width * 0.01, display_height * 0.86)
+    display_img(imgs.stand, display_width * 0.12, display_height * 0.86)
 
+
+# set x and y for first card dealt
+card1_x = (display_width * 0.35)
+card1_y = (display_height * 0.56)
+
+# begin main game loop
 while not end:
     for event in pyg.event.get():
         if event.type == pyg.QUIT:
             end = True
 
-    gameDisplay.fill(green)
-    display_img(imgs.Four_H, x, y)
+    setup()
 
     pyg.display.update()
     clock.tick(60)
--- FinalProject/imgs.py
+++ FinalProject/imgs.py
@@ -1,6 +1,12 @@
-# file to save card files as images to open in game
+# file to save card files as images to open in game (de-clutters main loop)
 
 import pygame as pyg
 
+# cards
+AC = pyg.transform.rotozoom(pyg.image.load('./card_images/AC.png'), 0, 1.2)
+AD = pyg.transform.rotozoom(pyg.image.load('./card_images/AD.png'), 0, 1.2)
 
-Four_H = pyg.transform.rotozoom(pyg.image.load('./card_images/4oH.png'), 0, 0.2)
+# buttons
+deal = pyg.transform.rotozoom(pyg.image.load('./card_images/deal.png'), 0, 1)
+hit = pyg.transform.rotozoom(pyg.image.load('./card_images/hit.png'), 0, 1.1)
+stand = pyg.transform.rotozoom(pyg.image.load('./card_images/stand.png'), 0, 1.1)
