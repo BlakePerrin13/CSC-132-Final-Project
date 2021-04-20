ee8cb01fd7188f00895eabb6b34eafccf0e7788a
BlakePerrin13 <bep020@latech.edu>
2021-04-19 23:58:04 -0500

    Added more to the player class. It holds pretty much everything in the game

--- FinalProject/ObjClasses.py
+++ FinalProject/ObjClasses.py
@@ -10,7 +10,8 @@ class Object:
 # val specifies value of card i.e. King is 10
 # ind tells the index in list of cards
 class Card(Object):
-    def __init__(self, x, y, name, val, ind):
+    def
+    __init__(self, x, y, name, val, ind):
         super().__init__(x, y)
         self.name = name
         self.val = val
